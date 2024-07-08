ffc7faefe366c47c36840157440eb9b897ec1d02
Daniil Kistanov <dkistanov@apple.com>
2024-07-07 22:26:47 -0700

    update post.py helper + update template

--- post.py
+++ post.py
@@ -1,18 +1,20 @@
 """
 A small cli to make either a TIL post or Regular Post
 """
+
 import sys
 from string import Template
 from datetime import datetime
 import os
 
+ALLOWED_TYPES = ["til", "blog", "bookmark"]
 
 with open("templates/post.template", "r") as template:
     t = Template(template.read())
 
-post_type = input("Is this a TIL or Regular Post? [til/post] ")
+post_type = input(f"Is this a TIL or Regular Post? [{'/'.join(ALLOWED_TYPES)}] ")
 
-if post_type not in ["til", "post"]:
+if post_type not in ALLOWED_TYPES:
     print("Please enter either 'til' or 'post'")
     sys.exit(0)
 
@@ -24,4 +26,4 @@ file_title = today + "-" + "-".join(title.split(" ")) + ".md"
 
 file_path = os.path.join("_posts", file_title)
 with open(file_path, "w") as f:
-    f.write(t.substitute(type=post_type, title=title, summary=summary))
+    f.write(t.substitute(type=post_type, title=title, summary=summary, category=post_type))
